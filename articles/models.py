d7f776178126cad8f47ccd33deccd574d212d739
rladbstlr87 <manylong@naver.com>
2025-03-24 12:11:29 +0900

    Comment modeling/migration

--- articles/models.py
+++ articles/models.py
@@ -7,5 +7,6 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-# class Comment(models.Model):
-#     pass
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
