655b8d1ad5507e8ef102afc1af1e5085f7d23dca
rladbstlr87 <manylong@naver.com>
2025-03-25 10:13:50 +0900

    forms

--- articles/forms.py
+++ articles/forms.py
@@ -4,9 +4,17 @@ from .models import Article, Comment
 
 # 유효성 검사때문에 이거 하느거다
 class ArticleForm(ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
     class Meta():
         model = Article
         fields = '__all__'
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control'})
+        }
 
 class CommentForm(ModelForm):
     class Meta():
